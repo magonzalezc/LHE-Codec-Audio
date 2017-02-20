c3e48a82d0818c9f767442b7d97b4c5ce7862efc
Marina <magonzalezc90@gmail.com>
2017-02-20 17:14:24 +0100

    LHE as DPCM

--- LHEquantizer.py
+++ LHEquantizer.py
@@ -204,6 +204,121 @@ def calculateHops(hop0, hop1, hop_number, max_sample, min_sample):
 
 
 #*******************************************************************************#
+#	Function calculateHops: This function calculates the hop assigned to a      #
+#	sample, according to the previous one in the following method. This         #
+#	is the LHE algorithm, so some knowledge about it is recommended to          #
+#	understand better what this function does.                                  #
+#	Input: Actual sample value, number of samples to the first hop, previous    #
+#	hop value, scaled maximum and minimum sample values.                        #
+#	Output: New hop value                                                       #
+#*******************************************************************************#
+
+def calculateHops_2(hop0, hop1, hop_number, max_sample, min_sample):
+	"""Returns the value of the new hop based on the calculations with
+	the previous one, actual sample value and distance to the first hop.
+
+	Parameters: Actual sample value, number of samples to the first hop,
+	previous hop value, scaled maximum and minimum sample values.
+
+	Exceptions: This function does not throw an exception.
+
+	"""
+
+	# Samples belong to the interval [-32768, 32767], so we move them to
+	# [0, 65535] to avoid mathematical problems
+
+
+	# esto lo quito porque ya he arreglado las muestras
+	#hop0 = hop0 - min_sample
+
+        #hop0 = hop0 - 32767
+
+	percent_range = 0.15 # Factor for positive and negative ratios
+	#rmax = 13.5 # Factor for ratio limits
+	rmax = 7 # Factor for ratio limits 128*r*r*r = 43K
+
+
+        # con rango=1 parece mejor. curioso
+	# 2->44.65 db
+	# 2.5->45.27 esta es la rmax mejor
+	# 3->45.04 db
+	# 3.1->44.86
+	# 3.25->44.82
+	# 3.5-> 44.82
+	# 4->44.65 db
+
+    # con rango =0.75 parece peor
+    # r=2.5 -> 44.09
+    # r=3 -> 44.13
+    # r=4 -> 44.09
+
+	hop_result = 0 # Final hop
+
+	# Ratio values for positive hops
+	#ratio_pos = pow(percent_range * abs((max_sample - min_sample - 1 - hop0)/(hop1)), 0.33333333)
+	#ratio_pos = pow(percent_range * abs((32767 - hop0)/(hop1)), 0.33333333)
+	ratio_pos = pow(percent_range * abs((65535 - hop0)/(hop1)), 0.33333333)
+
+	# Ratio values for negative hops
+	ratio_neg = pow(percent_range * abs((hop0)/(hop1)), 0.33333333)
+
+
+	ratio_pos= min (ratio_pos,rmax)
+	ratio_neg= min (ratio_neg,rmax)
+
+	# con ratio=2 obtengo 48.08 db
+	# con ratio=2.5 obtengo 47.18 db
+        #ratio_pos=2
+        #ratio_neg=2
+
+
+	# --- AMPLITUDES COMPUTATION --- #
+
+
+	# Amplitude of positive hops
+	h6 = hop1 * ratio_pos
+	h7 = h6 * ratio_pos
+	h8 = h7 * ratio_pos
+
+	# Amplitude of negative hops
+	h2 = hop1 * ratio_neg
+	h1 = h2 * ratio_neg
+	h0 = h1 * ratio_neg
+
+	# Hop result values
+	if hop_number == 4:
+		hop_result =  0
+	elif hop_number == 5:
+		hop_result = hop1
+	elif hop_number == 3:
+		hop_result = hop1
+	elif hop_number == 6:
+		hop_result = int(h6)
+	elif hop_number == 2:
+		hop_result = int(h2)
+	elif hop_number == 7:
+		hop_result = int(h7)
+	elif hop_number == 1:
+		hop_result = int(h1)
+	elif hop_number == 8:
+		hop_result = int(h8)
+	elif hop_number == 0:
+		hop_result = int(h0)
+
+	# Hop result limits
+	if (hop_result <= 0):
+		hop_result = 1
+	#if (hop_result > max_sample - min_sample):
+	#	hop_result = max_sample - min_sample - 1
+        if (hop_result > 65535):
+                hop_result = 65535
+	# We bring back the sample to the [-32768, 32767] interval
+
+	#hop_result = hop_result + min_sample
+	return hop_result
+
+
+#*******************************************************************************#
 #	Function getHops: This gets a specific hop list given the samples values.   #
 #	The hop value will be predicted with the previous one.                      #
 #	Input: scaled samples list, total number of samples, maximum and minimum    #
@@ -266,15 +381,16 @@ def getHops(samples, n_samples, max_sample, min_sample):
 		emin = max_sample # Current minimum prediction error
 		e2 = 0 # Computed error for each hop
 		finbuc = 0 # We can optimize the code below with this
+		delta = os - hop0
 
 		# Positive hops computation
-		if (os - hop0 >= 0):
+		if (delta >= 0):
 			for j in range (4, 9):
 				# We start checking the difference between the original amplitude and the cache
-				e2 = os - calculateHops(hop0, hop1, j, max_sample, min_sample)
+				hop_result = calculateHops_2(hop0, hop1, j, max_sample, min_sample)
+				e2 = delta - hop_result
 				if (e2 < 0):
 					e2 = - e2
-					finbuc = 1 # When error is negative, we get the hop we need
 				if (e2 < emin):
 					hop_number = j # Hop assignment
 					emin = e2
@@ -282,14 +398,16 @@ def getHops(samples, n_samples, max_sample, min_sample):
 						break
 				else:
 					break
+			hop_result = calculateHops_2(hop0, hop1, hop_number, max_sample, min_sample)
+			result[amp] = hop0 + hop_result # Final amplitude
 
 		# Negative hops computation. Same bucle as before
 		else:
 			for j in range(4, -1, -1):
-				e2 = calculateHops(hop0, hop1, j, max_sample, min_sample) - os
+				hop_result = calculateHops_2(hop0, hop1, j, max_sample, min_sample)
+				e2 = - delta - hop_result
 				if (e2 < 0):
 					e2 = - e2
-					finbuc = 1
 				if (e2 < emin):
 					hop_number = j
 					emin = e2
@@ -297,9 +415,10 @@ def getHops(samples, n_samples, max_sample, min_sample):
 						break
 				else:
 					break
+			hop_result = calculateHops_2(hop0, hop1, hop_number, max_sample, min_sample)
+			result[amp] = hop0 - hop_result # Final amplitude
 
 		# Assignment of final value
-		result[amp] = calculateHops(hop0, hop1, hop_number, max_sample, min_sample) # Final amplitude
 		hops[amp] = hop_number  # Final hop value
 
 		#error computation
